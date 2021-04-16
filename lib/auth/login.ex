27e1f1049a8fe506ae465d5c9217c939c257f2a3
ggandra <gandra.gabriel@hotmail.com>
2021-04-15 21:50:48 -0300

    Bearer Authorization

--- lib/auth/login.ex
+++ lib/auth/login.ex
@@ -11,6 +11,7 @@ defmodule Efinancas.Auth.Login do
     Bcrypt.verify_pass(params["password"], password_hash)
     |> handle_auth(user)
   end
+  defp match_password(nil, _params), do: {:error, "User not found"}
 
   defp handle_auth(true, user), do: {:ok, user}
   defp handle_auth(false, _user), do: {:error, "Password not match"}
--- lib/efinancas/guardian.ex
+++ lib/efinancas/guardian.ex
@@ -1,23 +1,22 @@
 defmodule Efinancas.Guardian do
   use Guardian, otp_app: :efinancas
 
-  def subject_for_token(user, claims) do
+  alias Efinancas.{User, Repo}
+
+  def subject_for_token(user, _claims) do
     sub = to_string(user.id)
     {:ok, sub}
   end
-  def subject_for_token(_, _) do
-    {:error, :reason_for_error}
+  def subject_for_token(reason_for_error) do
+    {:error, reason_for_error}
   end
 
   def resource_from_claims(claims) do
-    # Here we'll look up our resource from the claims, the subject can be
-    # found in the `"sub"` key. In `above subject_for_token/2` we returned
-    # the resource id so here we'll rely on that to look it up.
     id = claims["sub"]
-    resource = Efinancas.get_resource_by_id(id)
+    resource = Repo.get_by(User, id: id)
     {:ok,  resource}
   end
-  def resource_from_claims(_claims) do
-    {:error, :reason_for_error}
+  def resource_from_claims(reason_for_error) do
+    {:error, reason_for_error}
   end
 end
